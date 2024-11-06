98f850d9c8665f15cccabba56f0392a40319a83e
marcomole00 <marcomole00@gmail.com>
2024-11-06 11:55:11 +0100

    chore: clean up code

--- onic_netdev.c
+++ onic_netdev.c
@@ -359,15 +359,11 @@ static int onic_rx_poll(struct napi_struct *napi, int budget)
 		struct onic_rx_buffer *buf =
 			&q->buffer[desc_ring->next_to_clean];
 		struct sk_buff *skb;
-		dma_addr_t phys_addr;
-		dma_addr_t real_phys_addr = buf->dma_addr;
+		dma_addr_t dma_addr = buf->dma_addr;
 		int len = cmpl.pkt_len;
 		u8 *data;
-		phys_addr = (dma_addr_t) desc_ring->desc + QDMA_C2H_ST_DESC_SIZE * cmpl_ring->next_to_clean;
 
-		netdev_info(q->netdev, "phys_addr: %p, real_phys_addr: %p", (void *) phys_addr,  (void *) real_phys_addr);
-		
-		dma_sync_single_for_cpu(priv->pdev->dev.parent, real_phys_addr, len, DMA_FROM_DEVICE);
+		dma_sync_single_for_cpu(&priv->pdev->dev, dma_addr, len, DMA_FROM_DEVICE);
 
 		// data is the pointer to the data in the page, and its being passed into the sk_buff struct
 		/* maximum packet size is 1514, less than the page size */
